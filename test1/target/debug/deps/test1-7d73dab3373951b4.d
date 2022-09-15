9490fb97942fed7c3352e1d10cef9159e92c5c86
Andre Quiroa <60197413+Tranqpenguin@users.noreply.github.com>
2022-09-15 12:05:26 -0400

    Testing Rust github
    test test

---
+++ test1/target/debug/deps/test1-7d73dab3373951b4.d
@@ -0,0 +1,5 @@
+/home/andrequiroa/Rust-programs/test1/target/debug/deps/test1-7d73dab3373951b4.rmeta: src/main.rs
+
+/home/andrequiroa/Rust-programs/test1/target/debug/deps/test1-7d73dab3373951b4.d: src/main.rs
+
+src/main.rs:
---
+++ test1/target/debug/test1.d
@@ -0,0 +1,1 @@
+/home/andrequiroa/Rust-programs/test1/target/debug/test1: /home/andrequiroa/Rust-programs/test1/src/main.rs
