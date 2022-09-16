f68e6cac929206b6bc84d4abcc3235726acb448a
Andre Quiroa <andrejquiroa@gmail.com>
2022-09-15 22:30:23 -0400

    read file
    put the content of the file into a vector

--- fgroups/src/main.rs
+++ fgroups/src/main.rs
@@ -1,12 +1,22 @@
-//Andre Quiroa 
+
+use std::io::{BufRead, BufReader};
 use std::fs::File;
-use std::io::prelude::*;
 
-fn main() {
-    let mut file = File::open("test.txt").expect("File not found");
-    let mut data = String::new();
-    file.read_to_string(&mut data).expect("Error while reading file");
-    println!("{}", data);
+pub fn main() {
+    let mut fp: Vec<char> = Vec::new();
+    let mut names: Vec<char> = Vec::new();
+    let mut content: Vec<char> = Vec::new();
+    let file = BufReader::new(File::open("test.txt").expect("Unable to open file"));
 
+    for line in file.lines() {
+        for ch in line.expect("Unable to read line").chars() {
+            let mut space = ch.is_whitespace();
+            content.push(ch);
+            //println!("Character: {}", ch);
+        }
+    }
+    
+    println!("--------------------------------------");
+    println!("{:?}", content);
 }
 
