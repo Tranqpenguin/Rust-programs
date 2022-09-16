a3e179360e3dc71fb2ecf798a30ac7b0bba9b0b5
Andre Quiroa <andrejquiroa@gmail.com>
2022-09-15 20:43:22 -0400

    failed attempts
    some progress I guess.

--- fgroups/src/main.rs
+++ fgroups/src/main.rs
@@ -1,6 +1,15 @@
 //Andre Quiroa 
-
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    println!("Enter File Name: ");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    let mut file = File::open(b1.to_string()).expect("File not found");
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("Error while reading file");
+    println!("{}", data);
+
 }
+
