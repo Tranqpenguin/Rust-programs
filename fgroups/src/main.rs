227c4145fa8f4b8279f68d1030761e89286572e0
Andre Quiroa <andrejquiroa@gmail.com>
2022-09-15 21:06:48 -0400

    it reads
    i can read a file

--- fgroups/src/main.rs
+++ fgroups/src/main.rs
@@ -3,10 +3,7 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let mut line = String::new();
-    println!("Enter File Name: ");
-    let b1 = std::io::stdin().read_line(&mut line).unwrap();
-    let mut file = File::open(b1.to_string()).expect("File not found");
+    let mut file = File::open("test.txt").expect("File not found");
     let mut data = String::new();
     file.read_to_string(&mut data).expect("Error while reading file");
     println!("{}", data);
