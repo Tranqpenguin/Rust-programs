ca0e95d5427708a0263ed2a4e69f6c72918e3e1e
Andre Quiroa <60197413+Tranqpenguin@users.noreply.github.com>
2022-09-20 12:02:04 -0400

    Part A done
    part a finished and working

--- brightness/src/main.rs
+++ brightness/src/main.rs
@@ -8,10 +8,18 @@ fn main() {
     assert!(env::args().len() == 2);
     let img = GrayImage::read(input.as_deref()).unwrap();
     let mut denom = img.denominator as u32;
+    let mut sum = 0 as f32;
+    let mut counter = 1 as i128;
     for pixel in img.pixels {
-        let a = pixel.value;
-
-        println!("{:?}",a);
+        let a = pixel.value as f32 / denom as f32;
+        sum = a as f32 + sum as f32;
+        counter = counter as i128 + 1 as i128;
     }
+    let avg = sum as f32 / counter as f32;
+    println!("{:.3}",avg);
+
+    
+
+    
 
 }
