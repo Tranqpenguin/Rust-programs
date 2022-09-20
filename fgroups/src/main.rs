ee917fd679d3d703afbaf11d2918aca2e6a6efe2
Andre Quiroa <andrejquiroa@gmail.com>
2022-09-20 18:46:20 -0400

    Vects working
    got the Two different Vectors to work, one for fp's and one for names.

--- fgroups/src/main.rs
+++ fgroups/src/main.rs
@@ -2,36 +2,37 @@ use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::collections::HashMap;
 
-pub fn main() {
-    let mut map: HashMap<&str, &str> = HashMap::new();
-    let file = BufReader::new(File::open("test.txt").expect("Unable to open file"));
-    for line in file.lines() {
-        for ch in line.as_ref().expect("Unable to read line").chars() {
-            let space = ch.is_whitespace();
-            let astring = (ch);
-            if space == true {
-                for ch in line.expect("Unable to read line").chars() {
-                    let bstring = (ch);
-                    if space == true {
-                        map.insert(
-                            &astring.to_string(),
-                            &bstring.to_string(),
-                        );
-                        break;
-                    }
-                }
 
+pub fn main() {
+    let mut fp: Vec<char> = Vec::new();
+    let mut names: Vec<char> = Vec::new();
+    let mut content: Vec<char> = Vec::new();
+    let file = BufReader::new(File::open("test.txt").expect("Unable to open file"));
+    let mut switch= 1;
+    for line in file.lines() {
+        for ch in line.expect("Unable to read line").chars() {
+            let mut space = ch.is_whitespace();
+            if space {
+                if switch == 1{
+                    switch = 2;
+                }
+                else{
+                    switch = 1;
+                }
+            }
+            if switch == 1{
+                fp.push(ch);
+            }
+            if switch == 2{
+                names.push(ch);
             }
             //println!("Character: {}", ch);
-            //let groups1 = HashMap::from([groups::new(ch.to_string(), " ")]);
         }
-
-        for(a,b) in &map{
-            print!("{a}: \"{b}\"");
-        }
-        println!("--------------------------------------");
-        //println!("{:?}", content);
     }
+
+    println!("--------------------------------------");
+    println!("{:?}", fp);
+    println!("{:?}", names);
 }
 
 /*
