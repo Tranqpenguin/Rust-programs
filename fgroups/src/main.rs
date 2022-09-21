1de90470e44c3a456c02c4ba94ed69186c15dba5
Andre Quiroa <andrejquiroa@gmail.com>
2022-09-21 18:06:11 -0400

    le code
    yes the code looks like code

--- fgroups/src/main.rs
+++ fgroups/src/main.rs
@@ -1,52 +1,39 @@
-extern crate core;
-
-use std::io::{BufRead, BufReader};
-use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::vec::Vec;
 use std::collections::HashMap;
 
-
-pub fn main() {
-    //let mut groups = HashMan::new();
-    let mut fp: Vec<String> = Vec::new();
-    let mut names: Vec<String> = Vec::new();
-    let file = BufReader::new(File::open("test.txt").expect("Unable to open file"));
-    let mut switch= 1;
-    let mut word1 = String::new();
-    for line in file.lines() {
-        for ch in line.expect("Unable to read line").chars() {
-            let mut space = ch.is_whitespace();
-            if space {
-                if switch == 1{
-                    switch = 2;
-                }
-                else{
-                    switch = 1;
-                }
+fn ws(c:char) -> bool{
+    c == ' ' || c == '\t'
+}
+fn main() {
+    let mut fgroups_hashmap: HashMap<String, Vec<String>> = HashMap::new();
+    for line in io::stdin().lock().lines(){
+        let mut line_split = line.as_ref().unwrap().splitn(2,|c:char|ws(c));
+        let fp = line_split.next().unwrap().to_string();
+        let name = line_split.next().unwrap().to_string();
+        match fgroups_hashmap.get_mut(&fp){
+            None =>{
+                fgroups_hashmap.insert(fp.to_string(), vec![name]);
             }
-            if switch == 1{
-                let mut space = ch.is_whitespace();
-                if !space {
-                    fp.push(ch.to_string());
-                }
-            }
-            if switch == 2{
-                if !space {
-                    if ch.to_string() != "" {
-                        let a = ch.to_string();
-                        word1.push_str(&a);
-                    }
-                }
-                else{
-                    names.push(word1.to_string());
-                    word1 = String::new();
-                }
+            Some(names) => {
+                names.push(name);
             }
         }
     }
-    println!("--------------------------------------");
-    println!("{:?}", fp);
-    println!("{:?}", names);
+    let mut current_state = "";
+    for names in fgroups_hashmap.values(){
+        if names.len() >1{
+            print!("{}", current_state);
+            for name in names{
+                print!("{}", name.trim_start());
+            }
+        }
+        current_state = "\n"
+    }
 }
+
+
 
 /*
 let mut state = "somthing"
