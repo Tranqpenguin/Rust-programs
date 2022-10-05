d0e90a3a6ac7326810fd30a904e6f42ad8d07bdf
Andre Quiroa <andrejquiroa@gmail.com>
2022-10-04 23:08:36 -0400

    lab
    lab done

--- testing/target/debug/deps/testing-07c5043e4a556676.d
+++ testing/target/debug/deps/testing-07c5043e4a556676.d
@@ -1,5 +1,5 @@
-/home/andrequiroa/Rust-programs/testing/target/debug/deps/testing-07c5043e4a556676: src/lib.rs
+/home/andrequiroa/Desktop-Home/testing/target/debug/deps/testing-07c5043e4a556676: src/lib.rs
 
-/home/andrequiroa/Rust-programs/testing/target/debug/deps/testing-07c5043e4a556676.d: src/lib.rs
+/home/andrequiroa/Desktop-Home/testing/target/debug/deps/testing-07c5043e4a556676.d: src/lib.rs
 
 src/lib.rs:
