0cecd87736633517782abfcf0a8d142529879390
Andre Quiroa <andrejquiroa@gmail.com>
2022-10-24 21:33:37 -0400

    Workin good
    it be workin

--- iii/ppmtrans/src/main.rs
+++ iii/ppmtrans/src/main.rs
@@ -1,10 +1,10 @@
 mod rotate;
-mod CLI;
-use crate::CLI::Cli;
+mod cli;
+use crate::cli::Cli;
 use clap::Parser;
 use csc411_image::{GrayImage, Read};
 use array2::Array2;
-use crate::rotate::{rotate_180, rotate_90};
+use crate::rotate::{rotate_180, rotate_270, rotate_90, rotate_h, rotate_t, rotate_v};
 
 fn main() {
     // This takes all the arguments and put them in their proper member location
@@ -16,19 +16,40 @@ fn main() {
     let width : usize = img.width as usize;
     let height : usize = img.height as usize;
     let a2 = Array2::from_row_major(width, height, img.pixels);
-    println!("{}", a2.height());
-    println!("{}", a2.width());
+    let mut output = Vec::new();
     //just checking if i can use pattern properly
     if pattern == "rotate180"{
-        let mut output = Vec::new();
         output = rotate_180(&a2).to_vec();
         for a in output{
             println!("{:?}",a);
         }
     }
     else if pattern == "rotate90" {
-         let mut output = Vec::new();
         output = rotate_90(&a2).to_vec();
+        for a in output{
+            println!("{:?}",a);
+        }
+    }
+    else if pattern == "rotate270"{
+        output = rotate_270(&a2).to_vec();
+        for a in output{
+            println!("{:?}",a);
+        }
+    }
+    else if pattern == "rotateh"{
+        output = rotate_h(&a2).to_vec();
+        for a in output{
+            println!("{:?}",a);
+        }
+    }
+    else if pattern == "rotatev"{
+        output = rotate_v(&a2).to_vec();
+        for a in output{
+            println!("{:?}",a);
+        }
+    }
+    else if pattern == "rotatet"{
+        output = rotate_t(&a2).to_vec();
         for a in output{
             println!("{:?}",a);
         }
